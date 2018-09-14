a1c6fd5075ae0a98943df7fa2fd750b751d920aa
Vasilii_Bobkov <vasily-bobkow@yandex.ru>
2018-09-14 16:39:10 +0300

    Added operation info for LocalDataController

--- rest-service/src/main/java/com/mdbank/controller/LocalDataController.kt
+++ rest-service/src/main/java/com/mdbank/controller/LocalDataController.kt
@@ -1,13 +1,16 @@
 package com.mdbank.controller
 
 import com.mdbank.controller.dto.localdata.FormattedLocalData
-import com.mdbank.controller.dto.localdata.LocalDataRequestDto
 import com.mdbank.service.LocalDataService
+import io.swagger.annotations.ApiOperation
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.transaction.annotation.Transactional
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 import java.time.Year
 
 @RestController
@@ -17,6 +20,7 @@ class LocalDataController {
     private lateinit var localDataService: LocalDataService
 
     @GetMapping
+    @ApiOperation("Поиск данных для конкретной географической точки, года и метео параметра")
     @Transactional(readOnly = true)
     fun getLocalDataByPositionAndYear(@RequestParam latitude: Double,
                                       @RequestParam longitude: Double,
